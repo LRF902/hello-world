7399312aa4ba4ad96f198494d91114ce06121843
LRF902 <lrf902@126.com>
2019-09-24 17:20:35 +0800

    初始化提交

---
+++ renren-cloud/renren-register/src/main/java/io/renren/RegisterApplication.java
@@ -0,0 +1,29 @@
+/**
+ * Copyright (c) 2018 人人开源 All rights reserved.
+ *
+ * https://www.renren.io
+ *
+ * 版权所有，侵权必究！
+ */
+
+package io.renren;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * 注册中心
+ *
+ * @author Mark sunlightcs@gmail.com
+ * @since 1.0.0
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class RegisterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RegisterApplication.class, args);
+    }
+
+}
